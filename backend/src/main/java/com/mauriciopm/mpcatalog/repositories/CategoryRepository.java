5b9f55059cf211520f02d55bc4d2ec5fbded88be
Mauricio Pereira <m.pereira.menezes@gmail.com>
2024-07-25 14:15:43 -0300

    adicionado Caterory (Service e Repository)

---
+++ backend/src/main/java/com/mauriciopm/mpcatalog/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.mauriciopm.mpcatalog.repositories;
+
+import com.mauriciopm.mpcatalog.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+
+}
