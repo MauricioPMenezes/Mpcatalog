d24d76f6b147f4b9d0ca5395cac698c5379f8330
Mauricio Pereira <m.pereira.menezes@gmail.com>
2024-07-25 13:43:30 -0300

    tabela tb_caterories criada com sucesso

--- backend/src/main/java/com/mauriciopm/mpcatalog/entities/Category.java
+++ backend/src/main/java/com/mauriciopm/mpcatalog/entities/Category.java
@@ -16,8 +16,6 @@ public class Category {
     private Long id;
     private String name;
 
-    @ManyToMany(mappedBy = "categories")
-    private Set<Product> products =new HashSet<>();
 
 
 
@@ -46,9 +44,6 @@ public class Category {
         this.name = name;
     }
 
-    public Set<Product> getProducts() {
-        return products;
-    }
 
     @Override
     public boolean equals(Object o) {
