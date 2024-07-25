09057f82a217d32d0481323acec948ccd1777694
Mauricio Pereira <m.pereira.menezes@gmail.com>
2024-07-25 13:41:33 -0300

    reiniciando projeto para correção de bug

--- backend/src/main/java/com/mauriciopm/mpcatalog/controllers/ProductController.java
+++
@@ -1,9 +0,0 @@
-package com.mauriciopm.mpcatalog.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = "/products")
-public class ProductController {
-}
