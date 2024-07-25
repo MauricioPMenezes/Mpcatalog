ca8c32a9929487eb715017e7fc542ba30c49a6e5
Mauricio Pereira <m.pereira.menezes@gmail.com>
2024-07-25 11:41:53 -0300

    Project Created

---
+++ backend/mpcatalog/src/main/java/com/mauriciopm/mpcatalog/MpcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.mauriciopm.mpcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/mpcatalog/src/test/java/com/mauriciopm/mpcatalog/MpcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauriciopm.mpcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
