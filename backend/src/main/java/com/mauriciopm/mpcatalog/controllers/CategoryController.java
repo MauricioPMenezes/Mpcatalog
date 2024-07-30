d176de62ce07f7f7fa10bd9f6913d89ffebece0e
Mauricio Pereira <m.pereira.menezes@gmail.com>
2024-07-29 21:15:30 -0300

    Adicionado update Category e exceptions

--- backend/src/main/java/com/mauriciopm/mpcatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/mauriciopm/mpcatalog/controllers/CategoryController.java
@@ -3,6 +3,9 @@ package com.mauriciopm.mpcatalog.controllers;
 import com.mauriciopm.mpcatalog.dto.CategoryDTO;
 import com.mauriciopm.mpcatalog.services.CategoryService;
 import com.mauriciopm.mpcatalog.services.exceptions.DatabaseException;
+import com.mauriciopm.mpcatalog.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -35,6 +38,18 @@ public class CategoryController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
+    }
+
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> update(@PathVariable Long id,@Valid @RequestBody CategoryDTO dto) {
+        try {
+            dto = service.update(id,dto);
+            return ResponseEntity.ok(dto);
+
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado!");
+        }
     }
 
 
--- backend/src/main/java/com/mauriciopm/mpcatalog/services/CategoryService.java
+++ backend/src/main/java/com/mauriciopm/mpcatalog/services/CategoryService.java
@@ -5,6 +5,7 @@ import com.mauriciopm.mpcatalog.entities.Category;
 import com.mauriciopm.mpcatalog.repositories.CategoryRepository;
 import com.mauriciopm.mpcatalog.services.exceptions.DatabaseException;
 import com.mauriciopm.mpcatalog.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -47,4 +48,18 @@ public class CategoryService {
         return new CategoryDTO(entity);
     }
 
+    @Transactional
+    public CategoryDTO update(Long id,CategoryDTO dto) {
+        try {
+            Category entity = repository.getReferenceById(id);
+            entity.setName(dto.getName());
+            entity =repository.save(entity);
+            return  new CategoryDTO(entity);
+
+        }catch (EntityNotFoundException e){
+
+            throw new ResourceNotFoundException("ID "+id+" NOT FOUND");
+
+        }
+    }
 }
