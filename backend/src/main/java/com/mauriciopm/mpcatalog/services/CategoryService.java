755db1ce5702693c58b60dca7fbdebdf8b7faf5c
Mauricio Pereira <m.pereira.menezes@gmail.com>
2024-07-30 16:47:29 -0300

    busca por categoria Paginada

--- backend/src/main/java/com/mauriciopm/mpcatalog/services/CategoryService.java
+++ backend/src/main/java/com/mauriciopm/mpcatalog/services/CategoryService.java
@@ -8,6 +8,8 @@ import com.mauriciopm.mpcatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -75,6 +77,13 @@ public class CategoryService {
         }catch (DataIntegrityViolationException e){
             throw new DatabaseException("Falha de Integridade Referencial");
         }
+
+    }
+
+    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+        Page<Category> list = repository.findAll(pageRequest);
+        Page<CategoryDTO> result = list.map(x -> new CategoryDTO(x));
+        return result;
 
     }
 }
